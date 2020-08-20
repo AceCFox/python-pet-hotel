1a51e119eac6a8e261752d25b61fde65621c5762
Ace Fox <afox09@gmail.com>
2020-08-20 10:05:12 -0500

    dbsetup

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- db name: pet_hotel 
+
+CREATE TABLE "owner" ("ID" SERIAL PRIMARY KEY, 
+"name" VARCHAR(100));
+
+CREATE TABLE "pet" ("ID" SERIAL PRIMARY KEY, 
+"name" VARCHAR(100), 
+"owner_id" INT REFERENCES "owner",
+"breed" VARCHAR(100),
+"color" VARCHAR(100), 
+"checked-in" TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+"checked-out" TIMESTAMP );
